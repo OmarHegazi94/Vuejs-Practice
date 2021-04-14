a77c5d9b7e66426223ff7007aa6fe072a8275c6f
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-04-14 23:51:00 +0200

    added local custom directive + added 2 modifires + pass object to directive as value

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,71 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <h1>Built-in Directives</h1>
-        <p v-text=" 'Some text' "></p>
-        <p v-html=" '<strong>Some strong text</strong>' "></p>
-      </div>
-    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Built-in Directives</h1>
+                <p v-text="'Some text'"></p>
+                <p v-html="'<strong>Some strong text</strong>'"></p>
+            </div>
+        </div>
 
-    <div class="row mt-5">
-      <div class="col-12">
-        <h1>Custom Directives</h1>
-        <p v-highlight:background.delayed="'green'">Color this</p>
-      </div>
+        <div class="row mt-5">
+            <div class="col-12">
+                <h1>Custom Global Directives</h1>
+                <p v-highlight:background.delayed="'green'">Color this</p>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-12">
+                <h1>Custom Local Directives</h1>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Color this</p>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  
-}
-</script>
+    name: "App",
+    directives: {
+        "local-highlight": {
+            beforeMount(el, binding) {
+                let delay = 0;
+                if (binding.modifiers["delayed"]) {
+                  delay = 3000;
+                }
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+                if (binding.modifiers["blink"]) {
+                  let mainColor = binding.value.mainColor;
+                  let secondColor = binding.value.secondColor;
+                  let currentColor = mainColor;
+
+                  setTimeout(() => {
+                    setInterval( () => {
+                      currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+
+                      if (binding.arg == "background") {
+                          el.style.backgroundColor = currentColor;
+                      } else {
+                          el.style.color = currentColor;
+                      }
+                    }, binding.value.delay)
+                  }, delay);
+                }
+
+                else {
+                  setTimeout(() => {
+                      // el.style.background = binding.value;
+                      if (binding.arg == "background") {
+                          el.style.backgroundColor = binding.value.mainColor;
+                      } else {
+                          el.style.color = binding.value.mainColor;
+                      }
+                  }, delay);
+                }
+
+            },
+        },
+    },
+};
+</script>
