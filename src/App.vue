b1350299df347656edd5175d72a63b6eb825536c
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-04-15 00:12:27 +0200

    created custom filter with computed property

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,15 @@
                 <p>{{ $filters.toUppercase(text) }}</p>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-12">
+                <input type="text" v-model="filterText" />
+            </div>
+            <ul>
+                <li v-for="(fruit, index) in filteredFruits" :key="index">{{ fruit }}</li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -13,9 +22,18 @@
 export default {
     name: "App",
     data() {
-      return {
-        text: 'Hello there!'
-      }
-    }
+        return {
+            text: "Hello there!",
+            fruits: ["Apple", "Banana", "Mango", "Melon"],
+            filterText: "",
+        };
+    },
+    computed: {
+        filteredFruits() {
+            return this.fruits.filter((element) => {
+                return element.toLowerCase().match(this.filterText);
+            });
+        },
+    },
 };
 </script>
