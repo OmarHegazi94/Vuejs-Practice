d9777c973dab3876a2ee7bd7eeee6151bea1add5
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-04-15 21:19:47 +0200

    basic animation

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,37 @@
         <div class="row">
             <div class="col-12">
                 <h1>Animations</h1>
+                <hr />
+                <button @click="show = !show" class="btn btn-primary">show alert</button>
+                <br />
+                <br />
+                <transition name="fade">
+                    <div v-if="show" class="alert alert-primary">This is some Info</div>
+                </transition>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-
 export default {
     name: "App",
     data() {
         return {
-            text: "Hello there!",
+            show: false,
         };
     },
 };
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
