af40324950b77804274e6471b82d86e05342b976
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-04-15 21:45:13 +0200

    added slide animation

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,11 @@
                 <button @click="show = !show" class="btn btn-primary">show alert</button>
                 <br />
                 <br />
-                <transition name="fade">
+                <!-- <transition name="fade">
+                    <div v-if="show" class="alert alert-primary">This is some Info</div>
+                </transition> -->
+
+                <transition name="slide">
                     <div v-if="show" class="alert alert-primary">This is some Info</div>
                 </transition>
             </div>
@@ -35,5 +39,33 @@ export default {
 .fade-enter-from,
 .fade-leave-to {
     opacity: 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+    animation: slide-in 1s ease-out forwards;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+    animation: slide-out 1s ease-out forwards;
+}
+
+@keyframes slide-in {
+    from {
+        transform: translateY(20px)
+    }
+    to {
+        transform: translateY(0)
+    }
+}
+
+@keyframes slide-out {
+    from {
+        transform: translateY(0)
+    }
+    to {
+        transform: translateY(20px)
+    }
 }
 </style>
