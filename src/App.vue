3b474abb3efed318b0681774e5f026200b69393d
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-04-15 22:46:41 +0200

    installed axios

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,8 @@
     <div class="container mt-5">
         <div class="row">
             <div class="col-12">
-                <h1>Animations</h1>
-                <hr />
-                <button @click="show = !show" class="btn btn-primary">show alert</button>
-                <br />
-                <br />
-                <!-- <transition name="fade">
-                    <div v-if="show" class="alert alert-primary">This is some Info</div>
-                </transition> -->
-
-                <transition name="slide">
-                    <div v-if="show" class="alert alert-primary">This is some Info</div>
-                </transition>
+                <h1>HTTP with Axios</h1>
+                
             </div>
         </div>
     </div>
@@ -24,48 +14,9 @@ export default {
     name: "App",
     data() {
         return {
-            show: false,
+            
         };
     },
 };
 </script>
 
-<style scoped>
-.fade-enter-active,
-.fade-leave-active {
-    transition: opacity 0.5s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-    opacity: 0;
-}
-
-.slide-enter-active,
-.slide-leave-active {
-    animation: slide-in 1s ease-out forwards;
-}
-
-.slide-enter-from,
-.slide-leave-to {
-    animation: slide-out 1s ease-out forwards;
-}
-
-@keyframes slide-in {
-    from {
-        transform: translateY(20px)
-    }
-    to {
-        transform: translateY(0)
-    }
-}
-
-@keyframes slide-out {
-    from {
-        transform: translateY(0)
-    }
-    to {
-        transform: translateY(20px)
-    }
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 
 const app = createApp(App);
+
+app.use(VueAxios, axios);
 
 app.mount("#app");
