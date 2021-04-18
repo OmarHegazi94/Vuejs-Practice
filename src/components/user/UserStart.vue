7c02417898e97fc25e1d8590496e7d7930e959e0
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-04-18 20:04:16 +0200

    installed vue router + added some user components

---
+++ src/components/user/UserStart.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>Please select a User</p>
+        <hr>
+        <ul class="list-group">
+            <li class="list-group-item" style="cursor:pointer"></li>
+            <li class="list-group-item" style="cursor:pointer"></li>
+            <li class="list-group-item" style="cursor:pointer"></li>
+        </ul>
+    </div>
+</template>
