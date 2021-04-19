82e859c3b600b121e56433c56e6f95b987786903
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-04-19 22:36:00 +0200

    named routes

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
-    <Header></Header>
+    <!-- <Header></Header> -->
+    <router-view name="header-top"></router-view>
+    
     <div class="container mt-5">
         <div class="row">
             <div class="col-12">
                 <h1>Vue Router</h1>
                 <hr>
-                <router-view></router-view>
+                <router-view ></router-view>
+                <router-view name="header-bottom"></router-view>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import Header from './components/Header'
+// import Header from './components/Header'
 export default {
     name: "App",
     data() {
         return {}
     },
-    components: {
-        Header
-    }
+    // components: {
+    //     Header
+    // }
 };
 </script>
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,31 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "./components/Home.vue";
+import Header from "./components/Header.vue";
 import User from "./components/user/User.vue";
 import UserStart from "./components/user/UserStart.vue";
 import UserEdit from "./components/user/UserEdit.vue";
 import UserDetails from "./components/user/UserDetails.vue";
 
 const routes = [
-    { path: "/", component: Home, name: 'home' },
     {
-        path: "/user", component: User, children: [
-            { path: '', component: UserStart, name: 'userStart' },
-            { path: ':id', component: UserDetails, name: 'userDetails' },
-            { path: ':id/edit', component: UserEdit, name: 'userEdit' },
-        ]
+        path: "/",
+        name: "home",
+        components: {
+            default: Home,
+            "header-top": Header,
+        },
+    },
+    {
+        path: "/user",
+        components: {
+            default: User,
+            "header-bottom": Header,
+        },
+        children: [
+            { path: "", component: UserStart, name: "userStart" },
+            { path: ":id", component: UserDetails, name: "userDetails" },
+            { path: ":id/edit", component: UserEdit, name: "userEdit" },
+        ],
     },
 ];
 
