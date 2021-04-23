8f86940084fc5452f3697815aa738684e597ba54
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-04-23 02:32:41 +0200

    setting up basic example without vuex

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,27 @@
             <div class="col-12">
                 <h1>Vuex</h1>
                 <hr />
+                <app-result :counter="counter"></app-result>
+                <hr>
+                <app-counter @updateCount="counter += $event"></app-counter>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import Counter from './components/Counter'
+import Result from './components/Result'
 export default {
     name: "App",
     data() {
-        return {};
+        return {
+            counter: 0
+        };
     },
+    components: {
+        appCounter: Counter,
+        appResult: Result
+    }
 };
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <button class="btn btn-primary" @click="increment">Increment</button>
+      <button class="btn btn-primary" @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        increment() {
+            this.$emit('updateCount', 1);
+        },
+        decrement() {
+            this.$emit('updateCount', -1);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
