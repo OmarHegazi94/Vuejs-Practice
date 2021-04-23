cd1040a15d05f98e12cbce9979af33ea8781601e
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-04-23 02:56:21 +0200

    getters

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
                 <h1>Vuex</h1>
                 <hr />
                 <app-result></app-result>
+                <app-another-result></app-another-result>
                 <hr>
                 <app-counter></app-counter>
             </div>
@@ -15,11 +16,13 @@
 <script>
 import Counter from './components/Counter'
 import Result from './components/Result'
+import AnotherResult from './components/AnotherResult'
 export default {
     name: "App",
     components: {
         appCounter: Counter,
-        appResult: Result
+        appResult: Result,
+        appAnotherResult: AnotherResult
     }
 };
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,11 @@ import { createStore } from "vuex";
 export const store = createStore({
     state: {
         counter: 0
+    },
+    getters: {
+        doubleCounter: (state) => {
+            return state.counter * 2;
+        }
     }
 });
 
